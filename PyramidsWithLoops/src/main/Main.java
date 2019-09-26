27f0509c2dbfab64898c321441a1963bd626699f
alex5 <alex5@192.168.0.6>
2019-09-26 01:52:49 -0400

    thomp2aj_Assignment04

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,8 +1,10 @@
 /* 
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
- * What I really want is.
+ * Alexander Thompson
+ * Description: Loop Pyramid
+ * Due Date: 9/26/19 at 8 AM
+ * email:thomp2aj@mail.uc.edu
+ * Printing pyramids using loops. It works
+ *
         1
        222
       33333
@@ -14,12 +16,32 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int n = 5;
+		
+		int x;
+			
+		for (int i=0; i<n; i++) {
+			
+			//Inner Loop to set the spaces for each number
+			for (int j=n-i; j>1; j--){
+				//Below prints the actual spaces
+				System.out.print(" ");
 			}
-			System.out.println("");
+				x= i+1;
+				System.out.print(x);
+				
+	
+				
+				//Inner Loop to set the correct number of columns
+				//Each of these values will change due to outer loop
+				for (int j=1; j<=i; j++ ){
+					System.out.print(x + "" + x);
+					
+					
+				}
+				//Below is the ending line for every row
+			System.out.println();
 		}
 	}
 }
+
